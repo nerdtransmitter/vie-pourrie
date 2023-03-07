ac5b7b4c4af6da3115079b102e0cab92cea8a5c5
Martial Courcier <martialcourcier@sfr.fr>
2023-03-07 16:39:03 +0100

    pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit::Authorization
 
   protect_from_forgery with: :exception
 
@@ -11,5 +12,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :location, :email, :password) }
 
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :location, :email, :password,:current_password) }
+  end
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
