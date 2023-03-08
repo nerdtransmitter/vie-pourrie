49d088459be2559eaf928107c3ae42f96746b791
Alessia Moison <adm2217@columbia.edu>
2023-03-08 13:05:09 +0100

    preferences form et index

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,18 +1,21 @@
 class CardsController < ApplicationController
+
   def index
-    @cards = Card.all
+    if params[:outside].present?
+      if params[:active].present?
+        @cards = Card.where(outside: params[:outside]).where(active: params[:active])
+      else
+        @cards = Card.where(outside: params[:outside])
+      end
+    else
+      @cards = Card.all
+    end
   end
 
   def show
     @card = Card.find(params[:id])
   end
 
-  # def edit
-  #   @card = Card.find(params[:id])
-  # end
-
-  # # def update
-  # #   @card.
-  # #   @card = Card.
-  # # end
+  def filters
+  end
 end
