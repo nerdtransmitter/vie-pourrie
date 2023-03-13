3413076d8089c0a007495f410cd5efb77e7187dd
Martial Courcier <martialcourcier@sfr.fr>
2023-03-13 16:45:41 +0100

    animation

---
+++ app/javascript/controllers/cards_controller.js
@@ -0,0 +1,14 @@
+
+import { Controller } from "@hotwired/stimulus"
+// Connects to data-controller=“cards”
+export default class extends Controller {
+  static targets = ["card"]
+  connect() {
+    setTimeout(() => {
+      this.cardTargets.forEach(element => {
+        element.style.inset = 'unset'
+        element.style.margin = '0 auto'
+      });
+    }, 3000);
+  }
+}
