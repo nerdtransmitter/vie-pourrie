9aa6ad49a02bc9ad03bee0808931041a96e8049c
Olga Rusnac <olga.v.rusnac@gmail.com>
2023-03-14 18:23:45 +0100

    juste le modal du feedback, pas les modal du feeling du jour

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    # @journal_entry = JournalEntry.find(params[:id])
   end
 
   def start
