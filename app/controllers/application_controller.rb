70ed445d00243e63158d993fab9dcfcfcd7c44ec
Alessia Moison <adm2217@columbia.edu>
2023-03-15 10:01:29 +0100

    filename

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :location, :email, :password) }
 
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :location, :email, :password,:current_password) }
-  end
-
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
