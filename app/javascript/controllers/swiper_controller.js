f1be5914bf665f0ec17aa8c3a5d67f45c5ba8415
Alessia Moison <adm2217@columbia.edu>
2023-03-15 10:11:22 +0100

    heroku

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,20 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 // import Swiper and modules styles
 
 // Connects to data-controller="swiper"
 export default class extends Controller {
   connect() {
     console.log(this.element)
-    const swiper = new Swiper(this.element, {
-      modules: [Navigation, Pagination],
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    })
+    const swiper = new Swiper(this.element)
   }
 }
