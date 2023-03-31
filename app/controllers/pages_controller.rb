58596850db6ff0d6965dfed0565860af7e34fd9b
Alessia Moison <adm2217@columbia.edu>
2023-03-31 14:04:03 +0200

    cleaning

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    # @journal_entry = JournalEntry.find(params[:id])
     @mood = Mood.new
-    # Create a new Mood object and set the appropriate attributes
-    # Save the Mood object to the database
-    # Redirect to the appropriate page
   end
 
   def start
@@ -18,6 +14,5 @@ class PagesController < ApplicationController
     @journal_entries = current_user.journal_entries
     @journal_entries = @journal_entries.order('created_at DESC')
     @wishlists = current_user.wishlists
-    # @wishlist = Wishlist.find(params[:id])
   end
 end
